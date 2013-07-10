75d759786800b9a4a1a10574cb0df2ea0473e3a0
gorgotron <gorgotron@gorgotron.(none)>
2013-07-10 18:38:02 -0400

    webview + progress bar

--- gen/linux/android/club/fsoss/R.java
+++ gen/linux/android/club/fsoss/R.java
@@ -24,15 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_add=0x7f080006;
-        public static final int action_delete=0x7f080005;
-        public static final int action_save=0x7f080008;
-        public static final int action_settings=0x7f080007;
+        public static final int ProgressBar=0x7f080005;
+        public static final int action_add=0x7f080007;
+        public static final int action_delete=0x7f080006;
+        public static final int action_save=0x7f080009;
+        public static final int action_settings=0x7f080008;
         public static final int etNote=0x7f080002;
         public static final int etNoteTitle=0x7f080001;
         public static final int eventDummyText=0x7f080003;
         public static final int pager=0x7f080000;
-        public static final int webDummyText=0x7f080004;
+        public static final int webView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/linux/android/club/fsoss/WebFragment.java
+++ src/linux/android/club/fsoss/WebFragment.java
@@ -1,19 +1,49 @@
 package linux.android.club.fsoss;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebFragment extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 		View rootView = inflater.inflate(R.layout.fragment_web, container, false);
+		final ProgressBar progress = (ProgressBar) rootView.findViewById(R.id.ProgressBar);
 		
-		((TextView)rootView.findViewById(R.id.webDummyText)).setText("Web Portion here");
+        WebView webView = (WebView) rootView.findViewById(R.id.webView1);
+		
+        webView.getSettings().setJavaScriptEnabled(true);
+        
+        webView.setInitialScale(1);
+        
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        
+        
+        webView.setWebViewClient(new WebViewClient(){
+        	@Override
+        	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        		
+        	    progress.setVisibility(View.VISIBLE);
+        		super.onPageStarted(view, url, favicon);
+        	}
+        	
+        	@Override
+        	public void onPageFinished(WebView view, String url) {
+        	    progress.setVisibility(View.GONE);
+        		super.onPageFinished(view, url);
+        	}
+        });
+        webView.loadUrl("http://fsoss.senecac.on.ca/2013/");				
 		
 		return rootView;
 	}
